d90934d3aac2527148087a8dc3a48d0852ba5c91
Colby McHenry <colbymchenry@gmail.com>
2022-03-12 16:35:45 -0500

    dd

--- src/App.server.jsx
+++ src/App.server.jsx
@@ -8,14 +8,16 @@ import CartProvider from './components/CartProvider.client';
 
 function App({log, pages, ...serverState}) {
     console.log("TOKENS")
-    console.log(process.env.VITE_STORE_DOMAIN);
-    console.log(process.env.VITE_STORE_FRONT_TOKEN);
-    console.log(process.env.VITE_VERCEL_GIT_COMMIT_AUTHOR_NAME)
+    console.log(import.meta.env.VITE_STORE_DOMAIN);
+    console.log(import.meta.env.VITE_VITE_STORE_DOMAIN);
+    console.log(import.meta.env.VITE_STORE_FRONT_TOKEN);
+    console.log(import.meta.env.VITE_VITE_STORE_FRONT_TOKEN);
+    console.log(import.meta.env.VITE_VERCEL_GIT_COMMIT_AUTHOR_NAME)
   return (
     <Suspense fallback={<LoadingFallback />}>
       <ShopifyProvider shopifyConfig={{
-          storeDomain: process.env.VITE_STORE_DOMAIN,
-          storefrontToken: process.env.VITE_STORE_FRONT_TOKEN,
+          storeDomain: import.meta.env.VITE_STORE_DOMAIN,
+          storefrontToken: import.meta.env.VITE_STORE_FRONT_TOKEN,
           storefrontApiVersion: 'unstable',
       }}>
         <CartProvider>
