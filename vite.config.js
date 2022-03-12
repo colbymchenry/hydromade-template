43a33f5be63709903e9452d990e43a75be8a4364
Colby McHenry <colbymchenry@gmail.com>
2022-03-12 16:41:59 -0500

    Refix.

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import hydrogen from '@shopify/hydrogen/plugin';
 
 export default ({ mode }) => {
   require('dotenv').config();
+  // now you can access config with process.env.{configName}
 
   return defineConfig({
     optimizeDeps: {include: ['@headlessui/react']},
     plugins: [hydrogen({
-      storeDomain: import.meta.env.VITE_STORE_DOMAIN,
-      storefrontToken: import.meta.env.VITE_STORE_FRONT_TOKEN,
+      storeDomain: process.env.VITE_STORE_DOMAIN,
+      storefrontToken: process.env.VITE_STORE_FRONT_TOKEN,
       storefrontApiVersion: 'unstable',
     }), readableStreamWorkaround()],
   });
